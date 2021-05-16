02a896eb5487afbe4d88ed9e34c724a4222ecac3
beebuzzbuzz <43892847+beebuzzbuzz@users.noreply.github.com>
2021-05-16 11:57:25 -0700

    completed parts 1 and 2

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,40 @@
 
 import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
 const setState = router.setState;
+const gear = document.querySelector("img[alt=settings]");
+const newEntry = document.querySelector("h1");
+
 
 // Make sure you register your service worker here too
 
+newEntry.addEventListener('click', () => {
+  // if (history.state != null && history.state.name != 'home') {
+  setState({name: 'home'}, false);
+  // };
+});
+
+gear.addEventListener('click', () => {
+  setState({name: 'settings'},false);
+});
+
+window.addEventListener('popstate', (event) => {
+  setState(event.state, true);
+});
+
 document.addEventListener('DOMContentLoaded', () => {
+  let entryId = 0;
+  setState("home");
   fetch('https://cse110lab6.herokuapp.com/entries')
-    .then(response => response.json())
-    .then(entries => {
-      entries.forEach(entry => {
+    .then((response) => response.json())
+    .then((entries) => {
+      entries.forEach((entry) => {
         let newPost = document.createElement('journal-entry');
+        entryId++; 
         newPost.entry = entry;
+        newPost.id = entryId;
+        newPost.addEventListener('click', () => {
+          setState({name: 'entry', id: newPost.id}, false);
+        });
         document.querySelector('main').appendChild(newPost);
       });
     });
--- sw.js
+++ sw.js
@@ -4,3 +4,33 @@
 //   - One for installation
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
+
+
+const urlToCache = "https://cse110lab6.herokuapp.com/entries";
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open("my-site-cache-v1")
+        .then(function (cache) {
+            console.log("Opened cache");
+            return cache.add(urlToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function (response) {
+            if (response) {
+                return response;
+            }
+
+            return fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(clients.claim());
+});
